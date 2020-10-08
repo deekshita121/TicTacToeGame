37af78b55076f8667bfd5f0d4734c2a131773777
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-08 11:33:22 +0530

    [deekshita] Add :  showing Board for better choosing while playing

--- TicTacToeGame/src/TicTacToeGame.java
+++ TicTacToeGame/src/TicTacToeGame.java
@@ -20,6 +20,16 @@ public class TicTacToeGame {
 		return letter;
 	}
 	
+	public static void displayBoard()
+	{
+		char board[] = boardCreation();
+		System.out.println(" "+board[0]+" | "+board[1]+ " | "+board[2]+ " ");
+		System.out.println("-----------");
+		System.out.println(" "+board[3]+" | "+board[4]+ " | "+board[5]+ " ");
+		System.out.println("-----------");
+		System.out.println(" "+board[6]+" | "+board[7]+ " | "+board[8]+ " ");
+	}
+	
 	public static void main(String args[])
 	{
 		char computerLetter;
@@ -32,7 +42,7 @@ public class TicTacToeGame {
 		
 		System.out.println("Computer Letter "+computerLetter);
 		System.out.println("Player Letter "+playerLetter);
-		
+		displayBoard();
 	}
 	
 }
